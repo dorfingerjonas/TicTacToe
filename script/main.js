0cdcfde28c0ab9fcbff5a6233087516603575858
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-01-31 06:42:01 +0100

    add buildGrid() function

--- script/main.js
+++ script/main.js
@@ -1,3 +1,21 @@
 window.addEventListener('load', () => {
+function buildGrid() {
+    const wrapper = document.getElementById('gameWindow');
+    let row = document.createElement('div');
+
+    for (let i = 0; i < 9; i++) {
+        const cell = document.createElement('div');
+
+        cell.setAttribute('id', `cell${i+1}`);
+        cell.setAttribute('class', `cell`);
+        row.appendChild(cell);
+
+        if ((i+1) % 3 === 0) {
+            row.classList.add('row');
+            wrapper.appendChild(row);
+            row = document.createElement('div');
+        }
+    }
+}
 
 });
